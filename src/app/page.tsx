700ec717b7bd9d6750733032ce0d895e2fa704f8
VOLTA0007 <volta@gmail.com>
2024-05-07 19:43:47 +0300

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from '@/components/Hero'
 import { Hero2 } from '@/components/Hero2'
 import { Hero3 } from '@/components/Hero3'
+import { Hero4 } from '@/components/Hero4'
 import { NavBar } from '@/components/NavBar'
 
 
@@ -12,6 +13,7 @@ export default function Home() {
             <Hero />
             <Hero2 />
             <Hero3 />
+            <Hero4 />
         </main>
     )
 }
---
+++ src/components/Hero4.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Hero4 = () => {
+    return (
+        <section className="mt-[125px]">
+            <div className="flex justify-center items-center flex-col">
+                <p className="font-[700] text-black text-opacity-50 mb-5">
+                    OUR PROJECTS
+                </p>
+                <h1 className="font-[700] text-[30px] mb-12 text-center">
+                    Our amazing work with various Industry
+                </h1>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 mx-[30px] md:mx-[135px] gap-14">
+                <img src="/Dummy 4.png"></img>
+                <img src="/Dummy 5.png"></img>
+                <img src="/Dummy 6.png"></img>
+                <img src="/Dummy 7.png"></img>
+            </div>
+            <div className='flex justify-center items-center mt-10'>
+                <button className="w-[179px] h-[60px] rounded-[32px] border-[1px] border-[#F1F1F1]">
+                    See All
+                </button>
+            </div>
+        </section>
+    )
+}
