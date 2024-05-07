fa545798bbbb4f8b5698c30496871414adfb4926
VOLTA0007 <volta@gmail.com>
2024-05-07 23:16:35 +0300

    Commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer className="bg-[#111111] text-white w-full text-balance">
-            <div className="md:mx-[135px] mx-[15px]">
-                <div className="flex justify-between mb-10 px-5 py-14 max-xs:grid-cols-1 max-xs:grid max-xs:text-center">
+            <div className="mx-[15px]">
+                <div className="flex justify-between mb-10 px-5 py-14 max-x:grid-cols-1 max-x:grid max-x:text-center md:mx-[255px]">
                     <div>
                         <h1 className="font-[700] text-[36px]">
                             Let’s Collaborate With Us
@@ -13,13 +13,13 @@ export const Footer = () => {
                             We can’t wait to make amazing project with you
                         </p>
                     </div>
-                    <div className="flex justify-center items-center max-xs:mt-10">
+                    <div className="flex justify-center items-center max-x:mt-10">
                         <button className="flex justify-center items-center font-[700] text-[12px] text-black bg-gradient-to-r from-[#B57737] via-[#ECC974] to-[#F7D980] rounded-[75px] w-[150px] h-[50px] pt-[16px] pr-[32px] pb-[16px] pl-[32px]">
                             Get Started
                         </button>
                     </div>
                 </div>
-                <div className="grid  md:grid-cols-5 max-x:grid-cols-3 max-xs:grid-cols-1 gap-5 grid-cols-1 w-fit mx-auto py-10">
+                <div className="grid md:grid-cols-5 max-x:grid-cols-3 max-xs:grid-cols-1 gap-20 grid-cols-1 w-fit mx-auto py-10">
                     <div>
                         <img
                             className="max-xs:mb-5"
